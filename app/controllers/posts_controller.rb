69185b6ca0f59d83e3884cc9840043e21de3a7e9
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-25 12:53:51 -0400

    added post routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,51 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.order(created_at: :asc)
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to :posts
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to :posts
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :post_text)
   end
 
 end
